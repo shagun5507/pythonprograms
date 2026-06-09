afb21fd7811a91c69a394360db323ddd42a7b04d
vijendra12345 <vijendra.singh0682@gmail.com>
2026-06-09 11:21:39 +0530

    Initial commit

---
+++ conditionalstmnt.py
@@ -0,0 +1,149 @@
+# program of conditional stmt
+
+print("hello world........................")
+#
+# color ="red"
+# if(color == "green"):
+#     print("go")
+# elif(color == "red"):
+#     print("stop")
+# elif(color == "yellow"):
+#     print("wait")
+# else:
+#     print("choose proper color")
+#
+#     print("end of program")
+
+# per=33
+# if(per>=75):
+#     print("honours")
+# elif(per>=60 and per<75):
+#     print("firstdivision")
+# elif(per>=45 and per<50):
+#     print("seconddivision")
+# elif(per>=33 and per<45):
+#     print("third division ")
+# else:
+#     print("fail")
+
+# per=78
+# if(per>=78):
+#     print("honours")
+# elif(per>=60 and per <78):
+#     print("first division")
+# elif(per>=45 and per<60):
+#     print("second division")
+# elif(per>=33 and per<45):
+#     print("third division")
+# else:
+#         print("fail")
+#
+# color="green"
+# if (color == "red"):
+#     print("stop")
+# elif (color == "yellow"):
+#     print("wait")
+# elif(color == "green"):
+#     print("go")
+
+
+
+# color="pink"
+# if (color =="red"):
+#     print("stop")
+# elif(color=="green"):
+#     print("go")
+# elif(color=="yellow"):
+#     print("wait")
+# else:
+#     print("choose proper color")
+#
+
+# per=80
+# if(per>=75):
+#     print("honours")
+# elif(per>=60and per<75):
+#     print("first div")
+# elif(per>=45 and per<60):
+#     print("second div")
+# elif(per>=33 and per<45):
+#     print("third div")
+# else :
+#         print("fail")
+
+
+
+# per=85
+# if(per>=75):
+#     print("honours")
+# elif(per>=60 and per<75):
+#     print("first div")
+# elif(per>=45 and Per ,50):
+#     print("second div")
+# elif(per>=33and per<45):
+#     print("third div")
+# else: fail
+
+# color = "red"
+# if (color =="green"):
+#      print("go")
+# elif(color=="yellow"):
+#      print("wait")
+# elif(color=="red"):
+#      print("stop")
+
+
+
+
+
+
+
+
+
+# per=78
+# if(per>=75):
+#     print("honours")
+# elif(per>=60 and per<75):
+#     print("first div)")
+# elif(per>=45 and per<60):
+#     print("second div")
+# elif(per>=30 and per<45):
+#     print("third div")
+# else:
+#     print("fail")
+# color="green"
+# if (color=="red"):
+#     print("stop")
+# elif (color=="yellow"):
+#     print("stop")
+# elif (color=="green"):
+#     print("go")
+
+
+# color ="green"
+# if(color=="red"):
+#     print("stop")
+# elif (color=="yellow"):
+#     print("wait")
+# elif(color=="green"):
+#     print("go")
+
+
+# num1 = int(input("enter first number"))
+# num2 = int(input("enter second number"))
+# if num1 > num2:
+#     print("the num1 is greater than the number")
+# else:
+#     print("the num2 is less than the number")
+
+a =5
+b= 10
+a += b
+print(a)
+
+
+
+
+
+
+
